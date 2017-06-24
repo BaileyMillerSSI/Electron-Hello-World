948df88f72f6833af331f190c7cfe8d00b8c7d04
Bailey Miller <bmiller@susquehannasoftware.com>
2017-06-23 21:49:35 -0400

    Lots of improvements!

--- AngularProfile-Page/src/app/app.module.ts
+++ AngularProfile-Page/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes, ActivatedRoute } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent }
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +24,9 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularProfile-Page/src/app/login/login.component.ts
+++ AngularProfile-Page/src/app/login/login.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit
+{
 
-  constructor() { }
+  LoginForm: FormGroup;
+  LoginMessage: string;
 
-  ngOnInit() {
+  loading: boolean = false;
+
+  alertMessage: string;
+  alertVisible: boolean = false;
+  alertTimer: any
+
+  constructor(private fb: FormBuilder, private nav: Router)
+  {
+
+
+  }
+
+  closeAlert(): void
+  {
+    this.alertVisible = false;
+  }
+
+  Login(value: any)
+  {
+    this.loading = true;
+    this.closeAlert();
+    
+    setTimeout(() =>
+    {
+      this.loading = false;
+      if (this.alertTimer)
+      {
+        this.alertTimer = null;
+      }
+
+      var message = 3 == 3 ? "Cannot connect to login service." : "The error message from a login service would go here.";
+      this.alertMessage = message;
+      this.alertVisible = true;
+      this.alertTimer = setTimeout(() =>
+      {
+        this.closeAlert();
+      }, 10000);
+      this.ResetForm(this.LoginForm, false);
+    }, 5000);
+  }
+
+
+  public LoginButtonAppearance(): string
+  {
+    if (this.LoginForm.valid && !this.LoginForm.controls["RememberMe"].value && !this.loading) {
+      this.LoginMessage = "Sign in";
+      return "btn-warning";
+    } else if (this.LoginForm.valid && this.LoginForm.controls["RememberMe"].value && !this.loading) {
+      this.LoginMessage = "Sign in"
+      return "btn-success";
+    } else if (!this.LoginForm.valid && !this.loading) {
+      this.LoginMessage = "Validation Issues";
+      return "btn-danger";
+    }
+    else if (this.loading && this.LoginForm.valid) {
+      this.LoginMessage = "Logging in..."
+      return "btn-success";
+    }
+
+  }
+
+  private ResetForm(form: FormGroup, complete?: boolean)
+  {
+    if (complete) {
+      form.reset();
+    } else {
+      form.controls["Password"].reset();
+    }
+
+  }
+
+  ngOnInit()
+  {
+  this.LoginForm = this.fb.group({
+    'Email': ['', Validators.compose([Validators.email, Validators.required])],
+    'Password': ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+    'RememberMe': [false]
+  });
   }
 
 }

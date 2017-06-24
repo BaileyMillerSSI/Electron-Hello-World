3b64041954f9a63a97d02d3d636c2f78d78aa425
Bailey Miller <bmiller@susquehannasoftware.com>
2017-06-23 20:32:40 -0400

    Created the new profile page component

--- AngularProfile-Page/src/app/app.module.ts
+++ AngularProfile-Page/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
